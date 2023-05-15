e1eddd2bfb5c86b8ca7c71397437909cd914a5f9
Juan Solar <juansolarsolar@yahoo.com>
2023-05-15 16:54:56 -0400

    pruebas de cipiado de ejercicios

---
+++ ejercicio2.js
@@ -0,0 +1,3 @@
+// for in se recomienta usarlo para obtener las claves de un objeto, cuando queremos iterar sobre las claver de un objeto, no usarlo para arrays
+
+// for OfflineAudioCompletionEvent, para iterar en una coleccion  ordenada, donde no nos interesa la el indice
